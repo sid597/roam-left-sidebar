399d6b42c8e25959bbefac915ef0d82862c520db
sid597 <siddharthdv77@gmail.com>
2023-03-20 18:13:04 +0530

    show todos in the left sidebar as weblinks, when clicked takes to the block page. Also have a function to render these as blocks. Also modified the shortcuts code a bit with better modifier

--- src/main/left_sidebar/roam.cljs
+++ src/main/left_sidebar/roam.cljs
@@ -4,7 +4,7 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rd]
             [cljs.pprint :as pp]
-            [hickory.core :as h]
+            [clojure.string :as str]
             [left-sidebar.utils :as utils]
             ["@blueprintjs/core" :refer [Collapse Icon]]))
 
@@ -42,35 +42,73 @@
          [:div {:dangerouslySetInnerHTML {:__html html}}]]]])))
 
 (defn append-shortcuts-node []
-  (let [starred-pages-html (.-outerHTML (.querySelector js/document ".starred-pages"))]
-    (.remove (.querySelector js/document ".starred-pages"))
-    (rd/render [dropdown-menu starred-pages-html]
-               (.querySelector js/document ".title"))))
+  ;; Passing starred pages html because we have to replace it with a new one.
+  (let [starred-pages-html (.-outerHTML (.querySelector js/document ".starred-pages"))
+        sidebar-container (js/document.querySelector ".roam-sidebar-container .starred-pages-wrapper")]
+    (when sidebar-container
+      (.remove (.querySelector sidebar-container ".starred-pages"))
+      (rd/render [dropdown-menu starred-pages-html] (.querySelector sidebar-container ".title")))))
 
 
 
 ;; ========== My todos ==========
 
 
-(defn get-todos-for-user []
-  (let [user-uid (utils/get-uid-from-localstorage)
-        username (first (flatten (utils/q '[:find ?username
-                                            :in $ ?user-uid
-                                            :where
-                                            [?eid :user/uid ?user-uid]
-                                            [?eid :user/display-page ?duid]
-                                            [?duid :node/title ?username]]
-                                          user-uid)))]
-    (utils/q '[:find
-               (pull ?node [:block/string :node/title :block/uid])
-               (pull ?node [:block/uid])
-               (pull ?page [:node/title :block/uid :block/string])
-               :in $ ?username
-               :where
-               [?TODO-Ref :node/title "TODO"]
-               [?node-User-Display :node/title ?username]
-               [?node :block/refs ?TODO-Ref]
-               [?node :create/user ?node-User]
-               [?node-User :user/display-page ?node-User-Display]
-               [?node :block/page ?page]]
-             username)))
+(defn get-todos-for-user [username]
+    (->> (utils/q '[:find
+                    (pull ?node [:block/uid :block/string])
+                    :in $ ?username
+                    :where
+                    [?todo-eid :node/title "TODO"]
+                    [?user-eid :node/title ?username]
+                    [?node :block/refs ?todo-eid]
+                    [?node :create/user ?node-creator]
+                    [?node-creator :user/display-page ?user-eid]]
+                  username)
+         (mapv first)))
+
+
+(defn todo-item [block]
+  (let [graph-name (utils/get-graph-name)]
+    [:li
+     [:a {:href (str "/#/app/" graph-name "/page/" (:uid block))
+          :style {:text-decoration "none"}}
+      (-> block :string  str/trim)]]))
+
+
+(defn todos-list [todos]
+    (fn []
+      [:ul
+       (for [todo @todos]
+         ^{:key (:uid todo)} [todo-item todo])]))
+
+(defn todo-section [todos]
+  [:div.todos-sidebar-section
+   [:div.flex-h-box.title
+    [:span.bp3-icon-small.bp3-icon-tick]
+    [:div {:style {:flex "0 0 8px"}}]
+    [:span "TODOS"]]
+   [todos-list todos]])
+
+
+(defn start []
+  (let [user              (utils/get-current-user)
+        sidebar-container (js/document.querySelector ".roam-sidebar-container .starred-pages-wrapper")
+        todo-container    (js/document.createElement "div")
+        todos             (r/atom [])]
+    (.setAttribute todo-container "class" "todos-sidebar-container")
+    (when sidebar-container
+      (rd/render [todo-section todos] todo-container)
+      (.appendChild sidebar-container todo-container)
+      ;; This is a hack to get the todos to update, we can use
+      ;; mutation observers to do this better but it also complicates
+      ;; things.
+      (js/setInterval (fn []
+                        (reset! todos (get-todos-for-user user))) 1000))))
+
+(defn stop []
+  (let [sidebar-container (js/document.querySelector ".roam-sidebar-container .starred-pages-wrapper")
+        todo-container (js/document.querySelector ".todos-sidebar-container")]
+    (when sidebar-container
+      (rd/unmount-component-at-node todo-container)
+      (.removeChild sidebar-container todo-container))))
