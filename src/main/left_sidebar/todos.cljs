fe23c828d2a5e71d3751378d2a782a1c6c62fd2f
sid597 <siddharthdv77@gmail.com>
2023-03-22 21:43:23 +0530

    add input box for todos, which on click marks the todo done in graph

--- src/main/left_sidebar/todos.cljs
+++ src/main/left_sidebar/todos.cljs
@@ -1,5 +1,6 @@
 (ns left-sidebar.todos
   (:require [left-sidebar.utils :as utils]
+            [clojure.string :as str]
             ["@blueprintjs/core" :refer [Collapse Icon]]))
 (defn get-todos-for-user [username]
   (->> (utils/q '[:find
@@ -17,8 +18,17 @@
 (defn todo-item [block]
   [:a {:href (str "/#/app/resultsgraph/page/" (:uid block))
        :style {:text-decoration "none"}}
-   [:div
-    (:string block)]])
+   [:div {:class "todo-item"}
+
+    [:input {:type "checkbox"
+             :style {:margin-right "8px"
+                       :accent-color "black"}
+             :on-click #(.updateBlock (.-roamAlphaAPI js/window) (clj->js {:block
+                                                                           {:uid (:uid block)
+                                                                            :string (str/replace (:string block) "{{[[TODO]]}}" "{{[[DONE]]}}")}}))}]
+    (-> (:string block)
+        (str/replace "{{[[TODO]]}}" " "))]])
+
 
 (defn todos-component [todos]
   [:div {:class "todos"}
